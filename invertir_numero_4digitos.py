9bfb379723def8be624ed3c8be864358da849c3f
Chalarcanicolas <nicolaschalarca@colegioguanenta.edu.co>
2023-02-16 15:54:38 -0500

    archivo .py modificado

--- invertir_numero_4digitos.py
+++ invertir_numero_4digitos.py
@@ -6,10 +6,14 @@ A = int(input("se ingresa un numero de 4 digitos: "))
 
 # processing
 
-c4 = A % 10
-c3 = int((A % 100) / 10)
-c2 = int((A % 1000) / 100) 
-c1 = int((A - (A % 1000)) / 1000)
+c4 = (A % 10) * 1000
+pe = A//10
+c3 = int((A % 100) / 10) * 100
+pe = pe//10
+c2 = int((A % 1000) / 100) * 10
+c1 = pe // 10 
+
+nj  = c4 + c3 + c2 + c1
 
 # output
 
@@ -17,4 +21,4 @@ print("----------------------------------------")
 print("----------------RESULTADO---------------")
 print("----------------------------------------")
  
-print(" NUMERO INNVERSO: " + str(c4) + str(c3) + str(c2) + str(c1))
+print(" NUMERO INNVERSO: " + str(nj))
